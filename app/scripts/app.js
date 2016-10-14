81461a977ede9c505138d7792b69fe37a93f5b69
leog91 <leog91@gmail.com>
2016-10-13 21:12:06 -0300

    working on i18n

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,19 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'pascalprecht.translate',
+'tmh.dynamicLocale'
   ])
+  .constant('DEBUG_MODE', /*DEBUG_MODE*/true/*DEBUG_MODE*/)
+  .constant('VERSION_TAG', /*VERSION_TAG_START*/new Date().getTime()/*VERSION_TAG_END*/)
+  .constant('LOCALES', {
+    'locales': {
+      'ru_RU': 'Русский',
+      'en_US': 'English'
+    },
+    'preferredLocale': 'en_US'
+})
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -46,4 +57,27 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })// Angular debug info
+  .config(function ($compileProvider, DEBUG_MODE) {
+    if (!DEBUG_MODE) {
+      $compileProvider.debugInfoEnabled(false);// disables AngularJS debug info
+    }
+  })
+  // Angular Translate
+  .config(function ($translateProvider, DEBUG_MODE, LOCALES) {
+    if (DEBUG_MODE) {
+      $translateProvider.useMissingTranslationHandlerLog();// warns about missing translates
+    }
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'resources/locale-',
+      suffix: '.json'
+    });
+
+    $translateProvider.preferredLanguage(LOCALES.preferredLocale);
+    $translateProvider.useLocalStorage();
+  })
+  // Angular Dynamic Locale
+  .config(function (tmhDynamicLocaleProvider) {
+    tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
+});
