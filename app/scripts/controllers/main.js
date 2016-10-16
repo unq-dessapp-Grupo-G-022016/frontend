a0030416c87f35ef7ce8e354c37b7ebf6e4186c7
Leog <leog91@gmail.com>
2016-10-15 22:14:14 -0300

    working with httpGet & json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,30 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
+            $http.get("http://www.w3schools.com/angular/customers.php")
+                .then(function(response){
+                $scope.myWelcome = response.data;
+                $scope.myData = response.data.records;
+
+
+            });
+                      $http.get("http://validate.jsontest.com/?json=%5BJSON-code-to-validate%5D")
+                      .then(function(response){
+                          $scope.moreJson = "(1)" + response.data.object_or_array + "    seconds   " + response.data.parse_time_nanoseconds ;
+                      });
+
+
+
+
+                   /*   then(function(response){
+                $scope.content = response.data;
+                }, function(response){
+                $scope.content = "error";
+    }
+        );*/
+    
+    
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
