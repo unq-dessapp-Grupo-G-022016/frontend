5695873ceaedf7efba64d6e62b7f798af5848e17
Leog <leog91@gmail.com>
2016-11-01 05:32:12 -0300

    user  user&event json

--- app/scripts/controllers/dev.js
+++ app/scripts/controllers/dev.js
@@ -49,6 +49,39 @@ angular.module('frontendApp')
 
 
 
+    /*
+    
+        $scope.newUser =
+          {
+            userName: "momo24",
+            attendedEvents: [],
+            personalEvent: [],
+            profile: { categories: [] },
+            friends: { friends: [] },
+            lowCostTrip: { ammount: 50 }
+          };
+    
+    
+    
+        $scope.newUserWithEvent =
+          {
+            userName: "momo24",
+            attendedEvents: [{
+              id: 1, startTime: "2011-12-03T10:15:30",
+              category: { name: "warm places" }, endTime: "2011-12-03T10:15:30",
+              price: { ammount: 20 },
+              attenders: [],
+              details: "Devils house",
+              address: "666",
+              name: "goingToHell"
+            }],
+            personalEvent: [],
+            profile: { categories: [] },
+            friends: { friends: [] },
+            lowCostTrip: { ammount: 50000 }
+          };
+    */
+
 
     $scope.show = function (varr) {
 
--- app/scripts/services/userService.js
+++ app/scripts/services/userService.js
@@ -1,20 +1,26 @@
 'use strict';
 
 angular.module('frontendApp')
-  .service('userService', function($http, apiService ) {
-    return {
-        get: function(id){
-            return $http({
-                method: 'get',
-        //        url: apiService.url + "rest/user/read/" + id
-                url: "http://my-app-grupog.herokuapp.com/rest/user/read/" + id
-      
-                //url: "https://jsonplaceholder.typicode.com/posts?userId=1"
-         //       url: "http://my-app-grupog.herokuapp.com/rest/user/users"
-            });
-        }
-      
-      
-       
-    };
-});
+    .service('userService', function ($http, apiService) {
+        return {
+            get: function (id) {
+                return $http({
+                    method: 'get',
+                    //        url: apiService.url + "rest/user/read/" + id
+                    url: "http://my-app-grupog.herokuapp.com/rest/user/read/" + id
+
+                    //url: "https://jsonplaceholder.typicode.com/posts?userId=1"
+                    //       url: "http://my-app-grupog.herokuapp.com/rest/user/users"
+                });
+            },
+
+            save: function (newUser) {
+                return $http({
+                    method: 'post',
+                    url: 'http://my-app-grupog.herokuapp.com/rest/user/create',
+                    data: newUser
+                });
+            }
+
+        };
+    });
